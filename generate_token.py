2532c07ef59885cd14ca2e1f1aaa8c21bc1d96e3
vincius201413anos-collab <vincius201413anos@gmail.com>
2026-04-13 23:14:24 -0300

    Update generate_token.py

--- generate_token.py
+++ generate_token.py
@@ -1,8 +1,15 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
 
+SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
+
 flow = InstalledAppFlow.from_client_secrets_file(
     "credentials.json",
-    scopes=["https://www.googleapis.com/auth/youtube.upload"]
+    SCOPES
 )
 
 credentials = flow.run_local_server(port=0)
+
+with open("token.json", "w") as token:
+    token.write(credentials.to_json())
+
+print("token.json gerado com sucesso!")
