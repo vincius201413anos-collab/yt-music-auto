6194c1633eb833c7d900d690e1ad7344adacef6a
vincius201413anos-collab <vincius201413anos@gmail.com>
2026-04-14 05:06:45 -0300

    Create generate_youtube_token.py

---
+++ generate_youtube_token.py
@@ -0,0 +1,12 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
+
+flow = InstalledAppFlow.from_client_secrets_file(
+    "credentials.json",
+    SCOPES
+)
+
+creds = flow.run_local_server(port=0)
+
+print(creds.to_json())
