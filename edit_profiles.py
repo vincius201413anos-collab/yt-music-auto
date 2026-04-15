5293d6bdbbee8d39616d175b8fb1768a3790b34b
vincius201413anos-collab <vincius201413anos@gmail.com>
2026-04-14 21:01:44 -0300

    Create edit_profiles.py

---
+++ edit_profiles.py
@@ -0,0 +1,79 @@
+EDIT_PROFILES = {
+    "phonk": {
+        "zoom": 0.0012,
+        "max_zoom": 1.20,
+        "shake": True,
+        "flash": True,
+        "particles": "high"
+    },
+    "trap": {
+        "zoom": 0.0010,
+        "max_zoom": 1.15,
+        "shake": True,
+        "flash": True,
+        "particles": "medium"
+    },
+    "lofi": {
+        "zoom": 0.0003,
+        "max_zoom": 1.05,
+        "shake": False,
+        "flash": False,
+        "particles": "low"
+    },
+    "dark": {
+        "zoom": 0.0005,
+        "max_zoom": 1.08,
+        "shake": False,
+        "flash": False,
+        "particles": "low"
+    },
+    "electronic": {
+        "zoom": 0.0008,
+        "max_zoom": 1.12,
+        "shake": True,
+        "flash": True,
+        "particles": "medium"
+    },
+    "metal": {
+        "zoom": 0.0013,
+        "max_zoom": 1.22,
+        "shake": True,
+        "flash": True,
+        "particles": "high"
+    },
+    "pop": {
+        "zoom": 0.0006,
+        "max_zoom": 1.10,
+        "shake": False,
+        "flash": True,
+        "particles": "low"
+    },
+    "cinematic": {
+        "zoom": 0.0004,
+        "max_zoom": 1.07,
+        "shake": False,
+        "flash": False,
+        "particles": "medium"
+    },
+    "indie": {
+        "zoom": 0.0005,
+        "max_zoom": 1.09,
+        "shake": False,
+        "flash": False,
+        "particles": "low"
+    },
+    "rock": {
+        "zoom": 0.0009,
+        "max_zoom": 1.14,
+        "shake": True,
+        "flash": True,
+        "particles": "medium"
+    },
+    "default": {
+        "zoom": 0.0007,
+        "max_zoom": 1.10,
+        "shake": False,
+        "flash": False,
+        "particles": "low"
+    }
+}
