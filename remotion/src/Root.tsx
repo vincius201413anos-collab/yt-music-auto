54078f0bfe987e3f0b83c91e506c26a37861cead
vincius201413anos-collab <vincius201413anos@gmail.com>
2026-04-24 19:00:31 -0300

    Update Root.tsx

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -7,7 +7,7 @@ export const RemotionRoot = () => {
       <Composition
         id="MyComposition"
         component={MyComposition}
-        durationInFrames={300}
+        durationInFrames={1800} // 60 segundos (30fps)
         fps={30}
         width={1080}
         height={1920}
