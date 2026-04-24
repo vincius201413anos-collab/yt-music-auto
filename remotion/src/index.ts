9a3ff6555a728690783eda6f817d6538cd29b1de
vincius201413anos-collab <vincius201413anos@gmail.com>
2026-04-24 15:47:57 -0300

    Update index.ts

--- remotion/src/index.ts
+++ remotion/src/index.ts
@@ -1,4 +1,4 @@
 import { registerRoot } from "remotion";
-import { RemotionRoot } from "./Root";
+import { MyComposition } from "./Composition";
 
-registerRoot(RemotionRoot);
+registerRoot(MyComposition);
